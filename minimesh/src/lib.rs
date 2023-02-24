d7639288b3bc2b391b4eabee8a8da674f1287a8a
TristanAntonsen <tristan.antonsen@gmail.com>
2023-02-23 21:41:23 -0600

    mesh volume & area functions

--- minimesh/src/lib.rs
+++ minimesh/src/lib.rs
@@ -1,5 +1,23 @@
 
 use pyo3::prelude::*;
+use minimesh_lib::{Mesh, calculate_surface_area, calculate_volume};
+
+
+#[pyfunction]
+fn mesh_volume(path : &str) -> PyResult<f64> {
+
+    let stl_mesh = Mesh::from_stl(path);
+
+    Ok(calculate_volume(&stl_mesh))
+}
+
+#[pyfunction]
+fn mesh_area(path : &str) -> PyResult<f64> {
+
+    let stl_mesh = Mesh::from_stl(path);
+
+    Ok(calculate_surface_area(&stl_mesh))
+}
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -11,5 +29,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 #[pymodule]
 fn minimesh(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(mesh_volume, m)?)?;
+    m.add_function(wrap_pyfunction!(mesh_area, m)?)?;
     Ok(())
 }
