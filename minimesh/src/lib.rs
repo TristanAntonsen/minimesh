261a88ab4926c15a6710f0baa650ee22f3352926
TristanAntonsen <tristan.antonsen@gmail.com>
2023-02-26 19:22:28 -0600

    adding dimensions() and vertices()

--- minimesh/src/lib.rs
+++ minimesh/src/lib.rs
@@ -19,17 +19,45 @@ fn mesh_area(path : &str) -> PyResult<f64> {
     Ok(calculate_surface_area(&stl_mesh))
 }
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn dimensions(path : &str) -> PyResult<[f64; 3]> {
+
+    let stl_mesh = Mesh::from_stl(path);
+
+
+    Ok(stl_mesh.dimensions())
+}
+
+#[pyfunction]
+fn vertices(path : &str) -> PyResult<Vec<[f64; 3]>> {
+
+    let stl_mesh = Mesh::from_stl(path);
+
+    let vertices = stl_mesh.vertices.iter().map(|x| [x[0], x[1], x[2]]).collect();
+
+
+    Ok(vertices)
+}
+
+#[pyfunction]
+fn vertices(path : &str) -> PyResult<Vec<[f64; 3]>> {
+
+    let stl_mesh = Mesh::from_stl(path);
+
+    let vertices = stl_mesh.vertices.iter().map(|x| [x[0], x[1], x[2]]).collect();
+
+
+    Ok(vertices)
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn minimesh(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(mesh_volume, m)?)?;
     m.add_function(wrap_pyfunction!(mesh_area, m)?)?;
+    m.add_function(wrap_pyfunction!(dimensions, m)?)?;
+    m.add_function(wrap_pyfunction!(vertices, m)?)?;
     Ok(())
 }
+
+// https://pyo3.rs/v0.16.3/class/object
