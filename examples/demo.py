3389912c128704bca7a6327f55c8ddf1cd189cd6
TristanAntonsen <tristan.antonsen@gmail.com>
2023-02-26 19:35:07 -0600

    updated example

--- examples/demo.py
+++ examples/demo.py
@@ -1,7 +1,10 @@
 import minimesh as mm
 
 
-FILE_PATH = "stl/cube.stl"
+STL_PATH = "stl/cube.stl"
 
-print(f"STL Volume: {round(mm.mesh_volume(FILE_PATH),2)} mm^3")
-print(f"STL Surface area: {round(mm.mesh_area(FILE_PATH),2)} mm^2")
+print(f"STL Volume: {round(mm.volume(STL_PATH),2)} mm^3")
+print(f"STL Surface area: {round(mm.area(STL_PATH),2)} mm^2")
+print(f"STL Vertex count: {len(mm.vertices(STL_PATH))}")
+print(f"STL Tri count: {len(mm.triangles(STL_PATH))}")
+print(f"STL Dimensions: {[round(x, 2) for x in mm.dimensions(STL_PATH)]}")
